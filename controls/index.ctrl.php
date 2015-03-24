49664f4fd382711bc5ea34b6d51b6e050b170a43
dan-nl <d_entous@yahoo.com>
2015-03-24 14:13:24 +0100

    correcting the mustache references

--- controls/index.ctrl.php
+++ controls/index.ctrl.php
@@ -6,8 +6,8 @@
 	// @link https://github.com/bobthecow/mustache.php
 	$m = new Mustache_Engine(
 		array(
-			'loader' => new Mustache_Loader_FilesystemLoader( dirname( APPLICATION_PATH ) . '/app/vendor/europeana/ui-components' ),
-			'partials_loader' => new Mustache_Loader_FilesystemLoader( dirname( APPLICATION_PATH ) . '/app/vendor/europeana/ui-components' )
+			'loader' => new Mustache_Loader_FilesystemLoader( realpath( APPLICATION_PATH ) . '/vendor/europeana/ui-components' ),
+			'partials_loader' => new Mustache_Loader_FilesystemLoader( realpath( APPLICATION_PATH ) . '/vendor/europeana/ui-components' )
 		)
 	);
 
